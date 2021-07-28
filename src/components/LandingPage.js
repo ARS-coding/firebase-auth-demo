cc1fb304a4c588b439ceec546b1211fec01e6a2e
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-28 22:30:20 +0300

    Change the conditional rendering in landing page.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,19 +4,20 @@ import React from 'react'
 // router
 import { Link } from "react-router-dom";
 
+// react-redux
+import { useSelector } from "react-redux";
+
 // bootstrap
 import { Container } from "react-bootstrap";
 
 function LandingPage() {
+
+    const isSignedIn = useSelector(state => state.user.isSignedIn);
+
     return (
         <Container className="d-flex flex-column justify-content-center align-items-center" style={{minHeight: "80vh"}}>
-            <h1>Landing Page!</h1>
-            <p>
-            If you don't have an account <Link to="/sign-up">Sign up!</Link>
-            </p>
-            <p>
-            If you have an account <Link to="/sign-in">Sign in!</Link>
-            </p>
+            {isSignedIn && <h1>Landing Page!</h1>}
+            {!isSignedIn && <h1>Please sign up or sign in.</h1>}
         </Container>
     )
 }
