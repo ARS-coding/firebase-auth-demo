3ae7c8fc54b3dd26404d924709e45fa33fd77ecb
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-30 23:30:24 +0300

    Add formik as a dependency.

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -12,7 +12,7 @@ function ProfilePage() {
     // const hey = useParams() // use this to build paths on top of the /profile/:username such as /profile/:username/friends/:friendname 
 
     const uid = useParams().uid;
-
+    
     return (
         <Container className="justify-content-start align-items-center container-after-the-navbar" style={{minHeight: "90vh"}}>
             <h1>Welcome, {uid}!</h1>
