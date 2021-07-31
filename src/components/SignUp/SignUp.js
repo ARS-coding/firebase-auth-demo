001f8d07175e7d09ab41e9694dfb1e723c57e2da
Ali Rıza <ars.lifestyle@hotmail.com>
2021-08-01 00:22:23 +0300

    Remove all the logic that became unnecessary after implementing formik.

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,8 +1,7 @@
 // react
-import React, { useState } from 'react'
+import React from 'react'
 
 // bootstrap
-// import { Form, Button, Card, Container } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 
 // router
@@ -21,32 +20,8 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 function SignUp() {
 
     const initialFormData = { username: "", email: "", password: "", passwordConfirmation: "" };
-    const [formData, setFormData] = useState(initialFormData);
 
     let history = useHistory();
-
-    function handleFormSubmit(event) {
-        event.preventDefault();
-
-        if (formData.password === formData.passwordConfirmation) {
-            
-            const objWithoutPasswordConfigProp = removeOneProp(formData, "passwordConfirmation");
-            auth.createUserWithEmailAndPassword(formData.email, formData.password) 
-            .then(userCred => {
-                setUserDocument(userCred.user.uid, { ...objWithoutPasswordConfigProp, uid: userCred.user.uid })
-                return userCred;
-            }) // set the document in firestore
-            .then(userCred => history.push(`/profile/${userCred.user.uid}`)) // take the user to their profile
-            .catch(error => console.error("A problem occured while your account being created!", error));
-            setFormData(initialFormData);
-        } else {
-            console.log("Passwords are not the same.");
-        }
-    }
-
-    function handleFormChange(event) {
-        setFormData({ ...formData, [event.target.name]: event.target.value });
-    }
 
     return (
         <Container className="d-flex flex-column justify-content-center align-items-center" style={{minHeight: "90vh"}}>
@@ -165,58 +140,6 @@ function SignUp() {
                     )
                 }}
             </Formik>
-          
-            {/* <Card style={{width: "30%"}} className="ml-3 m-auto m-3">
-                <Card.Body>
-                    <h2 className="text-center">Sign Up</h2>
-                    <form onSubmit={handleFormSubmit}>
-                        <Form.Group id="email" className="my-3">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control 
-                                type="text"
-                                onChange={handleFormChange}
-                                name="username"
-                                value={formData.username}
-                                required 
-                            />
-                        </Form.Group>
-
-                        <Form.Group id="email" className="my-3">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control 
-                                type="email"
-                                onChange={handleFormChange}
-                                name="email"
-                                value={formData.email}
-                                required 
-                            />
-                        </Form.Group>
-
-                        <Form.Group id="password" className="my-3">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control
-                                type="password"
-                                onChange={handleFormChange}
-                                name="password"
-                                value={formData.password}
-                                required 
-                            />
-                        </Form.Group>
-
-                        <Form.Group id="password-confirm" className="my-3">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control 
-                                type="password"
-                                onChange={handleFormChange}
-                                name="passwordConfirmation"
-                                value={formData.passwordConfirmation}
-                                required 
-                            />
-                        </Form.Group>
-                        <Button type="submit" className="w-100">Sign Up!</Button>
-                    </form>
-                </Card.Body>
-            </Card> */}
             <div className="w-100 text-center mt-2">
                 Do you already have an account? <Link to="sign-in">Sign in!</Link>
             </div>
